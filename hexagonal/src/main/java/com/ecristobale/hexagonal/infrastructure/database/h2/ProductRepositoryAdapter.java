00d962fa288887e5ce4c57085e709e53d4b48d76
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-03-22 02:28:41 +0100

    Created JPA repository and injected in repository adapter, it is needed a mapper

--- hexagonal/src/main/java/com/ecristobale/hexagonal/infrastructure/database/h2/ProductRepositoryAdapter.java
+++ hexagonal/src/main/java/com/ecristobale/hexagonal/infrastructure/database/h2/ProductRepositoryAdapter.java
@@ -2,10 +2,18 @@ package com.ecristobale.hexagonal.infrastructure.database.h2;
 
 import com.ecristobale.hexagonal.domain.model.Product;
 import com.ecristobale.hexagonal.domain.repository.ProductRepositoryPort;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class ProductRepositoryAdapter implements ProductRepositoryPort {
+
+    private final ProductJpaRepository productJpaRepository;
+
     @Override
     public Product findProductByProductId(String productId) {
-        return null;
+        // This method is returning a domain model (Product) instead of an entity (ProductEntity)
+        // It is needed a mapper to convert from ProductEntity to Product
+        // A mapper is considered an anti corruption layer (in DDD)
+        return ProductJpaRepository.findByProductId(productId);
     }
 }
