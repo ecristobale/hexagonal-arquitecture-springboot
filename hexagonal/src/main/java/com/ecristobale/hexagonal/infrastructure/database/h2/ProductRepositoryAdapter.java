e6a9927532828fc2e9337230c06e4d29ac45c82b
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-03-22 02:29:01 +0100

    Created JPA repository and injected in repository adapter, it is needed a mapper

--- hexagonal/src/main/java/com/ecristobale/hexagonal/infrastructure/database/h2/ProductRepositoryAdapter.java
+++ hexagonal/src/main/java/com/ecristobale/hexagonal/infrastructure/database/h2/ProductRepositoryAdapter.java
@@ -14,6 +14,7 @@ public class ProductRepositoryAdapter implements ProductRepositoryPort {
         // This method is returning a domain model (Product) instead of an entity (ProductEntity)
         // It is needed a mapper to convert from ProductEntity to Product
         // A mapper is considered an anti corruption layer (in DDD)
-        return ProductJpaRepository.findByProductId(productId);
+        //return ProductJpaRepository.findByProductId(productId); <-- ERROR with return object
+        return null;
     }
 }
